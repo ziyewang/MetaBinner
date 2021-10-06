74961a74577637ebde6c8ab4049791fae0ff8b9a
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2021-10-06 13:22:42 +0200

    port auxiliary/FGS_gff.py to python3

--- auxiliary/FGS_gff.py
+++ auxiliary/FGS_gff.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import sys
 
 if len(sys.argv) < 3:
-	print sys.argv[0], "FGS-output new-GFF-file"
-	sys.exit()
+	print ("Usage:", sys.argv[0], "FGS-output new-GFF-file")
+	sys.exit(1)
 
 infile = open(sys.argv[1], "r")
 outfile = open(sys.argv[2], "w")
 
-print >>outfile, "##gff-version 3"
+outfile.write("##gff-version 3\n")
 for aline in infile:
 	aline = aline.strip()
 	if aline[0] == '>':
@@ -18,7 +18,7 @@ for aline in infile:
 		subs = aline.split()
 		id = seqid + "_" + subs[0] + "_" + subs[1] + "_" + subs[2]
 		des = [seqid, "FGS", "CDS", subs[0], subs[1], ".", subs[2], str(int(subs[3]) - 1), "ID=" + id + ";product=predicted protein"] 
-		print >>outfile, "\t".join(des)
+		outfile.write("\t".join(des) + "\n")
 outfile.close()
 infile.close()
 		
